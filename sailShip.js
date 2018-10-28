8c7e2cf244b09872aba9a6c0cff731ef4e3958c9
Chivalrybean <jlowmaster@gmail.com>
2018-10-27 22:48:07 -0700

    Initial building of classes to learn how they work.

--- sailShip.js
+++ sailShip.js
@@ -1,2 +1,14 @@
 //"Welcome to this project - Placeholder text so file won't be empty and not upload.";
+var room = /** @class */ (function () {
+    function room(roomName, background) {
+        this.name = roomName;
+    }
+    return room;
+}());
+var inventory = /** @class */ (function () {
+    function inventory(addItems) {
+        this.inv = addItems;
+    }
+    return inventory;
+}());
 //# sourceMappingURL=sailShip.js.map
--- sailShip.ts
+++ sailShip.ts
@@ -1,1 +1,15 @@
 //"Welcome to this project - Placeholder text so file won't be empty and not upload.";
+
+class room {
+  name: string;
+  constructor(roomName: string, background: string) {
+    this.name = roomName;
+  }
+}
+
+class inventory {
+  inv: Array<object>;
+  constructor(addItems: Array<object>) {
+    this.inv = addItems;
+  }
+}

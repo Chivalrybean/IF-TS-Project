30b0cd300938a35c03fa1c84a838f52eaf16fa45
Chivalrybean <jlowmaster@gmail.com>
2018-10-29 22:36:00 -0700

    Removed extranious class, added remaning classes.

--- sailShip.js
+++ sailShip.js
@@ -1,14 +1,58 @@
 //"Welcome to this project - Placeholder text so file won't be empty and not upload.";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    }
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var room = /** @class */ (function () {
-    function room(roomName, background) {
-        this.name = roomName;
+    function room(name, background, inventory, exits) {
+        this.name = name;
+        this.background = background;
+        this.inventory = inventory;
+        this.exits = exits;
+        this.active = false;
     }
     return room;
 }());
-var inventory = /** @class */ (function () {
-    function inventory(addItems) {
-        this.inv = addItems;
+var person = /** @class */ (function () {
+    function person(name, inventory, statuses) {
+        this.name = name;
+        this.inventory = inventory;
+        this.statuses = statuses;
     }
-    return inventory;
+    return person;
 }());
+var thingy = /** @class */ (function () {
+    function thingy(name) {
+        this.name = name;
+    }
+    return thingy;
+}());
+var trinket = /** @class */ (function (_super) {
+    __extends(trinket, _super);
+    function trinket(name, description) {
+        var _this = _super.call(this, name) || this;
+        _this.description = description;
+        return _this;
+    }
+    return trinket;
+}(thingy));
+var device = /** @class */ (function (_super) {
+    __extends(device, _super);
+    function device(name, description, action) {
+        var _this = _super.call(this, name) || this;
+        _this.description = description;
+        _this.action = action;
+        return _this;
+    }
+    return device;
+}(thingy));
 //# sourceMappingURL=sailShip.js.map
--- sailShip.ts
+++ sailShip.ts
@@ -2,14 +2,55 @@
 
 class room {
   name: string;
-  constructor(roomName: string, background: string) {
-    this.name = roomName;
+  background: string;
+  inventory: Array<object>;
+  exits: Array<string>;
+  active: boolean;
+    constructor(name: string, background: string, inventory: Array<object>, exits: Array<string>) {
+      this.name = name;
+      this.background = background;
+      this.inventory = inventory;
+      this.exits = exits;
+      this.active = false;
   }
 }
 
-class inventory {
-  inv: Array<object>;
-  constructor(addItems: Array<object>) {
-    this.inv = addItems;
+
+class person {
+  name: string;
+  inventory: Array<object>;
+  statuses: Array<string>;
+    constructor(name: string, inventory: Array<object>, statuses: Array<string>) {
+      this.name = name;
+      this.inventory = inventory;
+      this.statuses = statuses;
   }
+
+}
+
+class thingy {
+  name: string;
+    constructor(name: string) {
+      this.name = name;
+    }
+}
+
+class trinket extends thingy{
+  description: string;
+  constructor(name: string, description: string) {
+    super(name);
+    this.description = description;
+    }
+
+}
+
+class device extends thingy{
+  description: string;
+  action: string;
+  constructor(name: string, description: string, action: string) {
+    super(name);
+    this.description = description;
+    this.action = action;
+    }
+
 }
